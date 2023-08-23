02c876bbefefc154f6421d3d61bcbf3b6d63bb82
Alvin <alvinvaati@gmail.com>
2023-08-23 17:43:36 +0300

    lifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+a class LIFOCache that inherits from BaseCaching and is a caching system:
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO Cache class"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data
+
+    def put(self, key, item):
+        """Adds element and removes first element if cache is full"""
+        if not key or not item:
+            return
+        self.cache_data.update({key: item})
+        if len(self.cache_data.items()) > self.MAX_ITEMS:
+            last_item = [x for x in self.cache_data.keys()]
+            last_item.reverse()
+            self.cache_data.pop(last_item[1])
+            print('DICARD: {}'.format(last_item[1]))
+
+    def get(self, key):
+        """gets item from cache by key"""
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
