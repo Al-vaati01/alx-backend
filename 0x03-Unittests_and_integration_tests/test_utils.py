1b7ff006ab644c6518245e48f1aa14ba662c907e
Alvin <alvinvaati@gmail.com>
2023-08-28 12:12:29 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ Parameterized a unit test
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from typing import Dict, List, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -13,13 +14,21 @@ class TestAccessNestedMap(unittest.TestCase):
     test access class
     """
     @parameterized.expand(
-            [
-                ({"a": 1}, ["a", ], 1),
-                ({"a": {"b": 2}}, ["a", ], 2),
-                ({"a": {"b": 2}}, ["a", "b"], 2)
-            ]
+        [
+            ({"a": 1}, ["a", ], 1),
+            ({"a": {"b": 2}}, ["a", ], 2),
+            ({"a": {"b": 2}}, ["a", "b"], 2)
+        ]
     )
-    def test_access_nested_map(self, nest_map, path, expected_result):
+    def test_access_nested_map(
+            self,
+            nest_map: Dict[str, Any],
+            path: List[str],
+            expected_result: Any
+    ) -> None:
+        """
+        Test the access_nested_map function
+        """
         result = access_nested_map(nest_map, path)
         self.assertEqual(result, expected_result)
 
