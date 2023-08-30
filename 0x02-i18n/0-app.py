e63c3754b7531575534652f24cd32a9dec0e6903
Alvin <alvinvaati@gmail.com>
2023-08-30 13:22:14 +0300

    doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-Simple Flask i18n App
+"""Simple Flask i18n App
 This script creates a basic internationalization (i18n) web application using
 Flask,
 a micro web framework for Python. The app defines a single route for the root
