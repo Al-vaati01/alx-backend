9ab1cbfd0d4a68baa7cfc676f8769c0f5acf5496
Alvin <alvinvaati@gmail.com>
2023-08-30 13:18:36 +0300

    module doc

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """
 Simple Flask i18n App
-This script creates a basic internationalization (i18n) web
-application using Flask
+This script creates a basic internationalization (i18n) web application using
+Flask,
+a micro web framework for Python. The app defines a single route for the root
+URL ("/"),
+which renders an HTML template named '0-index.html'. This template can be
+customized
+for different languages or content.
 """
 from flask import Flask
 from flask import render_template
