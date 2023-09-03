f6d2c36fffcaaa01abcae355b13f7af394dddd19
Alvin <alvinvaati@gmail.com>
2023-09-03 11:26:33 +0300

    lru

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -6,21 +6,35 @@ from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRU Caching clas"""
+    """LRU Caching class"""
 
     def __init__(self):
         super().__init__()
-        self.cache_data = {}
         self.priority = []
 
     def put(self, key, item):
         if not key or not item:
             return
-        self.cache_data.update({key: item})
-        if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
-            for _k in self.cache_data.keys()
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.priority.pop(0)
+            del self.cache_data[lru_key]
+            print("DISCARD:", lru_key)
+
+        self.cache_data[key] = item
+        self.update_priority(key)
+
     def get(self, key):
-        """gets item from cache by key"""
-        if key is None or key not in self.cache_data.keys():
+        """Gets item from cache by key"""
+        if key is None or key not in self.cache_data:
             return None
-        return self.cache_data.get(key)
+
+        self.update_priority(key)
+
+        return self.cache_data[key]
+
+    def update_priority(self, key):
+        """Updates the priority of a key"""
+        if key in self.priority:
+
+            self.priority.remove(key)
+        self.priority.append(key)
